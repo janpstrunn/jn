993c55d0f946e590266f9afe29c4f5f11ee11e00
Janpstrunn <janpstrunn@disroot.org>
2025-07-08 14:42:45 -0300

    refactor!: Combine all script into one
    - BREAKING CHANGE: Combine all scripts into one
    - Add template feature
    - Add special symbol substitution (filename header)

--- lib/files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function display_all_files() {
-  find "$ZK_PATH" -type f | sed -E "s|$ZK_PATH||"
-}
-
-function display_workspaces() {
-  printf '%s\n' "${ZK_WORKSPACES[@]}"
-}
